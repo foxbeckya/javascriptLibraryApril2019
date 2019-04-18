1b9e1c0787e09507e9ffa04fb1597b83b5d2059b
foxbeckya <foxbeckya@gmail.com>
2019-04-17 21:03:45 -0400

    added 02.02folder

---
+++ 1-JavaScript-Fundamentals/classFiles/02.02/notesAndChallenges.js
@@ -0,0 +1,13 @@
+let arr = [{key: 'academy'}, undefined, true, ['brie', 'rob', [true, false], undefined], 1150];
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[3][2][1]);
+
+// arr[1] = 'whatever i want';
+
+// console.log(arr[1]);
+
+for (let i = 0; i < arr.length; i++){
+  console.log(arr[i]);
+}
